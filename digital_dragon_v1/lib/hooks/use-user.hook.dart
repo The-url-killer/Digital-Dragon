aefdf8928904b041b98a633dd21111a5dfc9941c
Diuliano Vargas Da Silveira <diuliano.silveira@cwi.com.br>
2022-03-08 19:28:24 -0300

    feat (0001) deixando dinamico as requisicoes

--- digital_dragon_v1/lib/hooks/use-user.hook.dart
+++ digital_dragon_v1/lib/hooks/use-user.hook.dart
@@ -1,24 +1,15 @@
-import 'dart:convert';
+import 'package:digital_dragon_v1/hooks/use-requisitions.hook.dart';
 
-import 'package:http/http.dart' as http;
-
-Future<String> login({email, password}) async {
-  var url = Uri.http('localhost:8080', '/sign-in', {'q': '{http}'});
-  var login = json.encode({'email': email, 'password': password});
-
-  await http
-      .post(url, body: login, headers: {"Content-Type": "application/json"});
+dynamic login({email, password}) async {
+  post(url: '/sign-in', body: {email, password});
 
   return email;
 }
 
 Future<String> signUp({name, email, password}) async {
-  var url = Uri.http('localhost:8080', '/sign-up', {'q': '{http}'});
-  var signUP =
-      json.encode({'name': name, 'email': email, 'password': password});
-
-  await http
-      .post(url, body: signUP, headers: {"Content-Type": "application/json"});
+  post(
+      url: '/sign-up',
+      body: {'name': name, 'email': email, 'password': password});
 
   return email;
 }
